8af737d9ca95db810a1f5b25891c82859b668e25
Moch Asef Saefudin <57564538+fesaworld@users.noreply.github.com>
2023-05-13 19:55:19 +0700

    basic widget - basic_layout - column_widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:fic4_flutter_widgets_challenges/tugas/basic_form/dialog_widget.d
 import 'package:fic4_flutter_widgets_challenges/tugas/basic_form/form_widget.dart';
 import 'package:fic4_flutter_widgets_challenges/tugas/basic_layout/aspect_ratio_widget.dart';
 import 'package:fic4_flutter_widgets_challenges/tugas/basic_layout/center_widget.dart';
+import 'package:fic4_flutter_widgets_challenges/tugas/basic_layout/column_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,7 +22,9 @@ class MyApp extends StatelessWidget {
       ),
       // home: const DialogWidget(),
       // home: FormWidget(),
-      home: const CenterWidget(),
+      // home: const AspectRatioWidget(),
+      // home: const CenterWidget(),
+      home: const ColumnWidget(),
     );
   }
 }
